e3be858dfe8c48a66f407d90873352bd7780128d
Leonardo Laranjeira <leonardolaranjeiradev@gmail.com>
2024-08-21 12:11:58 -0300

    Criado os Getters and Setters do Funcionario

--- Exercicio_List/src/entities/Funcionario.java
+++ Exercicio_List/src/entities/Funcionario.java
@@ -11,6 +11,33 @@ public class Funcionario {
 		this.nome = nome;
 		this.salario = salario;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	
+	
 	
 	
 	
